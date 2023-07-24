ba08f76cf143d555daef11c22ba77203e166c05d
Betül Kırçıl <ceng.betulkircil@gmail.com>
2023-07-25 00:02:47 +0300

    [DEV][Betul.Kircil][added firebase authentication dependency provider ]

--- app/src/main/java/com/betulkircil/cryptoinsight/di/AppModule.kt
+++ app/src/main/java/com/betulkircil/cryptoinsight/di/AppModule.kt
@@ -1,10 +1,16 @@
 package com.betulkircil.cryptoinsight.di
 
+import com.google.firebase.auth.FirebaseAuth
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModulee {
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideFireBaseAuth() = FirebaseAuth.getInstance()
 }
