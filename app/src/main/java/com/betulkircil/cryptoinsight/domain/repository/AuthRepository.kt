ecec85a1c0d1253ce7911d4ebaebef88aa8e6a6c
Betül Kırçıl <ceng.betulkircil@gmail.com>
2023-07-26 16:38:55 +0300

    [UPD][Betul.Kircil][updated authRepository interface to authentication ]

--- app/src/main/java/com/betulkircil/cryptoinsight/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/betulkircil/cryptoinsight/domain/repository/AuthRepository.kt
@@ -1,9 +1,37 @@
 package com.betulkircil.cryptoinsight.domain.repository
 
 import com.betulkircil.cryptoinsight.utils.Resource
+import com.betulkircil.cryptoinsight.utils.Response
 import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.StateFlow
 
+typealias SignUpResponse = Response<Boolean>
+typealias SendEmailVerificationResponse = Response<Boolean>
+typealias SignInResponse = Response<Boolean>
+typealias ReloadUserResponse = Response<Boolean>
+typealias SendPasswordResetEmailResponse = Response<Boolean>
+typealias RevokeAccessResponse = Response<Boolean>
+typealias AuthStateResponse = StateFlow<Boolean>
 interface AuthRepository {
-    suspend fun signInUser(email:String, password:String) : kotlinx.coroutines.flow.Flow<Resource<AuthResult>>
-    suspend fun signUpUser(email: String, password: String) : kotlinx.coroutines.flow.Flow<Resource<AuthResult>>
+    val currentUser: FirebaseUser?
+
+    suspend fun firebaseSignUpWithEmailAndPassword(email: String, password: String): SignUpResponse
+
+    suspend fun sendEmailVerification(): SendEmailVerificationResponse
+
+    suspend fun firebaseSignInWithEmailAndPassword(email: String, password: String): SignInResponse
+
+    suspend fun reloadFirebaseUser(): ReloadUserResponse
+
+    suspend fun sendPasswordResetEmail(email: String): SendPasswordResetEmailResponse
+
+    fun signOut()
+
+    suspend fun revokeAccess(): RevokeAccessResponse
+
+    fun getAuthState(viewModelScope: CoroutineScope): AuthStateResponse
+    //suspend fun signInUser(email:String, password:String) : kotlinx.coroutines.flow.Flow<Resource<AuthResult>>
+    //suspend fun signUpUser(email: String, password: String) : kotlinx.coroutines.flow.Flow<Resource<AuthResult>>
 }
