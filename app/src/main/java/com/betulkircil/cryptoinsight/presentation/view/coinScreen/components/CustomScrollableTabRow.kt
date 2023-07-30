3601766a6f319cd61706080c9665da2185792c40
Betül Kırçıl <ceng.betulkircil@gmail.com>
2023-07-30 19:47:50 +0300

    [UPD][Betul.Kircil][updated tabView's scrollable composable ]

---
+++ app/src/main/java/com/betulkircil/cryptoinsight/presentation/view/coinScreen/components/CustomScrollableTabRow.kt
@@ -0,0 +1,67 @@
+package com.betulkircil.cryptoinsight.presentation.view.coinScreen.components
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScrollableTabRow
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRowDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.betulkircil.cryptoinsight.R
+
+@Composable
+fun CustomScrollableTabRow(
+    tabs: List<String>,
+    selectedTabIndex: Int,
+    onTabClick: (Int) -> Unit
+) {
+    val density = LocalDensity.current
+    val tabWidths = remember {
+        val tabWidthStateList = mutableStateListOf<Dp>()
+        repeat(tabs.size) {
+            tabWidthStateList.add(0.dp)
+        }
+        tabWidthStateList
+    }
+    ScrollableTabRow(
+        selectedTabIndex = selectedTabIndex,
+        contentColor = Color.Black,
+        backgroundColor = Color.Transparent,
+        edgePadding = 0.dp,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                color = colorResource(id = R.color.purple_protest),
+                modifier = Modifier.customTabIndicatorOffset(
+                    currentTabPosition = tabPositions[selectedTabIndex],
+                    tabWidth = tabWidths[selectedTabIndex]
+                )
+            )
+        }
+    ) {
+        tabs.forEachIndexed { tabIndex, tab ->
+            Tab(
+                selected = selectedTabIndex == tabIndex,
+                onClick = { onTabClick(tabIndex) },
+                text = {
+                    Text(
+                        text = tab,
+                        style = MaterialTheme.typography.body2,
+                        fontWeight = FontWeight.Light,
+                        onTextLayout = { textLayoutResult ->
+                            tabWidths[tabIndex] =
+                                with(density) { textLayoutResult.size.width.toDp() }
+                        }
+                    )
+                }
+            )
+        }
+    }
+}
