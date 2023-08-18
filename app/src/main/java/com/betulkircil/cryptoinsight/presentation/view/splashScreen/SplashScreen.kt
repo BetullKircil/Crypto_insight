d735cacb1c8273e5b184351d361255da5fca637f
Betül Kırçıl <ceng.betulkircil@gmail.com>
2023-08-18 14:44:53 +0300

    [DEV][Betul.Kircil][added splash screen animation effect]

--- app/src/main/java/com/betulkircil/cryptoinsight/presentation/view/splashScreen/SplashScreen.kt
+++ app/src/main/java/com/betulkircil/cryptoinsight/presentation/view/splashScreen/SplashScreen.kt
@@ -1,6 +1,9 @@
 package com.betulkircil.cryptoinsight.presentation.view.splashScreen
 
+import android.view.animation.OvershootInterpolator
 import android.window.SplashScreen
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -8,8 +11,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
@@ -20,9 +25,29 @@ import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        Animatable(0f)
+    }
     LaunchedEffect(key1 = true){
+        scale.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
         delay(1000L)
         navController.navigate(Screen.HomeScreen.route)
     }
-    SplashScreenLogo()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        contentAlignment = Alignment.Center,
+    ) {
+        Image(painter = painterResource(id = R.drawable.logo), contentDescription = null, modifier = Modifier.scale(scale.value))
+
+    }
 }
--- app/src/main/java/com/betulkircil/cryptoinsight/presentation/view/splashScreen/components/SplashScreenLogo.kt
+++ app/src/main/java/com/betulkircil/cryptoinsight/presentation/view/splashScreen/components/SplashScreenLogo.kt
@@ -1,10 +1,12 @@
 package com.betulkircil.cryptoinsight.presentation.view.splashScreen.components
 
+import androidx.compose.animation.Animatable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,7 +16,9 @@ import com.betulkircil.cryptoinsight.R
 @Composable
 fun SplashScreenLogo() {
     Box(
-    modifier = Modifier.fillMaxSize().background(Color.Black),
+    modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Black),
     contentAlignment = Alignment.Center,
 ) {
     Image(painter = painterResource(id = R.drawable.logo), contentDescription = null)
