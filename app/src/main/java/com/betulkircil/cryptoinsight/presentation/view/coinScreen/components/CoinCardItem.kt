843cd470dd0df104340f3a2e2b373161f5a488f6
Betül Kırçıl <ceng.betulkircil@gmail.com>
2023-09-09 00:50:51 +0300

    [FIX][Betul.Kircil][fixed saved coins screen feature]

--- app/src/main/java/com/betulkircil/cryptoinsight/presentation/view/coinScreen/components/CoinCardItem.kt
+++ app/src/main/java/com/betulkircil/cryptoinsight/presentation/view/coinScreen/components/CoinCardItem.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberImagePainter
@@ -42,7 +43,7 @@ fun CoinCardItem(
         Toast
             .makeText(
                 context,
-                "Coins saved succesfully!",
+                "${coin.symbol.toUpperCase()} saved succesfully!",
                 Toast.LENGTH_SHORT
             )
             .show()
--- app/src/main/java/com/betulkircil/cryptoinsight/presentation/view/savedScreen/components/savedCoinsScreen.kt
+++ app/src/main/java/com/betulkircil/cryptoinsight/presentation/view/savedScreen/components/savedCoinsScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -70,7 +71,7 @@ fun SavedCoins(
                                     Toast
                                         .makeText(
                                             context,
-                                            "Coin deleted succesfully!",
+                                            "${coins.symbol.toUpperCase()} deleted succesfully!",
                                             Toast.LENGTH_SHORT
                                         )
                                         .show()
