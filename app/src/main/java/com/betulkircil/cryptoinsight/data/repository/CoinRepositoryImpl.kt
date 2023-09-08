fe0beeed913a2c1fa6ea975c9b644fd0c1e72450
Betül Kırçıl <ceng.betulkircil@gmail.com>
2023-09-08 23:30:59 +0300

    [DEV][Betul.Kircil][created favorite coins repository interface ]

--- app/src/main/java/com/betulkircil/cryptoinsight/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/betulkircil/cryptoinsight/data/repository/CoinRepositoryImpl.kt
@@ -1,14 +1,31 @@
 package com.betulkircil.cryptoinsight.data.repository
 
+import com.betulkircil.cryptoinsight.data.local.room.db.FavoriteCoinsDatabase
+import com.betulkircil.cryptoinsight.data.local.room.db.FavoriteNewsDatabase
 import com.betulkircil.cryptoinsight.data.remote.CoinsApi
 import com.betulkircil.cryptoinsight.data.remote.dto.CoinsDtoItem
+import com.betulkircil.cryptoinsight.domain.model.Coins
 import com.betulkircil.cryptoinsight.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class CoinRepositoryImpl @Inject constructor(
-    private val coinsApi: CoinsApi
+    private val coinsApi: CoinsApi,
+    private val coinsDb: FavoriteCoinsDatabase
 ): CoinRepository {
     override suspend fun getCoins(currencyString: String): List<CoinsDtoItem> {
         return  coinsApi.getCoins(currencyString)
+    }
+
+    override suspend fun upsert(coins: Coins): Long {
+        return coinsDb.getFavoriteCoinsDao().upsert(coins)
+    }
+
+    override fun getSavedCoins(): Flow<List<Coins>> {
+        return coinsDb.getFavoriteCoinsDao().getAllFavoriteCoins()
+    }
+
+    override suspend fun deleteSavedCoins(coins: Coins) {
+        return coinsDb.getFavoriteCoinsDao().deleteFavoriteCoins(coins)
     }
 }
--- app/src/main/java/com/betulkircil/cryptoinsight/domain/repository/CoinRepository.kt
+++ app/src/main/java/com/betulkircil/cryptoinsight/domain/repository/CoinRepository.kt
@@ -1,7 +1,13 @@
 package com.betulkircil.cryptoinsight.domain.repository
 
 import com.betulkircil.cryptoinsight.data.remote.dto.CoinsDtoItem
+import com.betulkircil.cryptoinsight.domain.model.Coins
+import com.betulkircil.cryptoinsight.domain.model.NewsModel
+import kotlinx.coroutines.flow.Flow
 
 interface CoinRepository {
     suspend fun getCoins(currencyString : String) : List<CoinsDtoItem>
+    suspend fun upsert(coins : Coins): Long
+    fun getSavedCoins() : Flow<List<Coins>>
+    suspend fun deleteSavedCoins(coins : Coins)
 }
