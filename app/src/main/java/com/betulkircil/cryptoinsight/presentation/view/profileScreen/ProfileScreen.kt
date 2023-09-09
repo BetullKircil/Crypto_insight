c364adc35f8c11ad6d55e7d6727b0f50a0c36be1
Betül Kırçıl <ceng.betulkircil@gmail.com>
2023-09-09 13:41:25 +0300

    [UPD][Betul.Kircil][updated log out alert dialog ui]

--- app/src/main/java/com/betulkircil/cryptoinsight/presentation/view/profileScreen/ProfileScreen.kt
+++ app/src/main/java/com/betulkircil/cryptoinsight/presentation/view/profileScreen/ProfileScreen.kt
@@ -2,11 +2,13 @@ package com.betulkircil.cryptoinsight.presentation.view.profileScreen
 
 
 import android.app.AlertDialog
+import android.app.Dialog
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -15,20 +17,38 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -47,10 +67,13 @@ fun ProfileScreen(
     navController: NavController,
     loginViewModel: LoginViewModel = hiltViewModel(),
 ) {
+    val openDialogCustom = remember { mutableStateOf(false) }
+
     val isLoggedIn = remember {
         mutableStateOf(false)
     }
     val context = LocalContext.current
+    val openAlertDialog = remember { mutableStateOf(false) }
 
 Scaffold(
         modifier = Modifier
@@ -138,30 +161,7 @@ Scaffold(
                     Box(modifier = Modifier
                         .fillMaxSize()
                         .clickable {
-                            val alertDialogBuilder = AlertDialog.Builder(context)
-                            alertDialogBuilder.setTitle("Warning")
-                            alertDialogBuilder.setMessage("Are you sure you would like to log out?")
-                            alertDialogBuilder.setPositiveButton("Yes") { dialog, which ->
-                                loginViewModel.logout()
-                                Toast
-                                    .makeText(
-                                        context,
-                                        "Logged out successfully!",
-                                        Toast.LENGTH_SHORT
-                                    )
-                                    .show()
-                                navController.navigate("loginScreen") {
-                                    popUpTo("profileScreen") { inclusive = true }
-                                }
-                            }
-                            alertDialogBuilder.setNegativeButton("Cancel") { dialog, which ->
-                                Toast
-                                    .makeText(context, "Log out is cancelled!", Toast.LENGTH_SHORT)
-                                    .show()
-                            }
-                            alertDialogBuilder
-                                .create()
-                                .show()
+                            openDialogCustom.value = true
                         }) {
                         ProfileOptions(sectionTitle = "Log Out", sectionText = "Log out from your account", pngRes = R.drawable.pp_logout, onClick = {
 
@@ -171,7 +171,11 @@ Scaffold(
             }
         }
     )
+    if (openDialogCustom.value) {
+        CustomDialogUI(openDialogCustom = openDialogCustom, navController = navController, loginViewModel = loginViewModel)
+    }
 }
+
 
 fun Context.sendMail(to: String, subject: String) {
     try {
@@ -184,5 +188,107 @@ fun Context.sendMail(to: String, subject: String) {
         Log.d("Error",e.message.toString())
     } catch (t: Throwable) {
         Log.d("Error",t.message.toString())
+    }
+}
+
+@Composable
+fun CustomDialogUI(modifier: Modifier = Modifier, openDialogCustom: MutableState<Boolean>, loginViewModel:LoginViewModel, navController: NavController){
+    val context = LocalContext.current
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(350.dp),
+        contentAlignment = Alignment.Center) {
+        Card(
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier
+                .padding(horizontal = 20.dp),
+            elevation = 8.dp
+        ) {
+            Column(
+                modifier
+                    .background(Color.White)
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.pp_logout),
+                    contentDescription = null,
+                    contentScale = ContentScale.Fit,
+                    colorFilter = ColorFilter.tint(
+                        color = colorResource(id = R.color.purple_protest)
+                    ),
+                    modifier = Modifier
+                        .padding(top = 20.dp)
+                        .height(70.dp)
+                        .fillMaxWidth(),
+
+                    )
+                Column(modifier = Modifier.padding(16.dp)) {
+                    androidx.compose.material3.Text(
+                        text = "Warning",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .padding(top = 5.dp)
+                            .fillMaxWidth(),
+                        style = androidx.compose.material3.MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    androidx.compose.material3.Text(
+                        text = "Are you sure you would like to log out?",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .padding(top = 10.dp, start = 25.dp, end = 25.dp)
+                            .fillMaxWidth(),
+                        style = MaterialTheme.typography.body2
+                    )
+                }
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp)
+                        .background(color = colorResource(id = R.color.morado_purple)),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    androidx.compose.material3.TextButton(onClick = {
+                        openDialogCustom.value = false
+                    }) {
+                        androidx.compose.material3.Text(
+                            "Yes",
+                            fontWeight = FontWeight.Bold,
+                            color = colorResource(id = R.color.grey_black_bg),
+                            modifier = Modifier.padding(top = 5.dp, bottom = 5.dp).clickable {
+                                loginViewModel.logout()
+                                Toast
+                                    .makeText(
+                                        context,
+                                        "Logged out successfully!",
+                                        Toast.LENGTH_SHORT
+                                    )
+                                    .show()
+                                navController.navigate("loginScreen") {
+                                    popUpTo("profileScreen") { inclusive = true }
+                                }
+                            }
+                        )
+                    }
+                    androidx.compose.material3.TextButton(onClick = {
+                        openDialogCustom.value = false
+                    }) {
+                        androidx.compose.material3.Text(
+                            "Cansel",
+                            fontWeight = FontWeight.ExtraBold,
+                            color = colorResource(id = R.color.grey_black_bg),
+                            modifier = Modifier.padding(top = 5.dp, bottom = 5.dp).clickable {
+                                Toast
+                                    .makeText(context, "Log out is cancelled!", Toast.LENGTH_SHORT)
+                                    .show()
+                                openDialogCustom.value = false
+                            }
+                        )
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/betulkircil/cryptoinsight/presentation/view/savedScreen/components/savedCoinsScreen.kt
+++ app/src/main/java/com/betulkircil/cryptoinsight/presentation/view/savedScreen/components/savedCoinsScreen.kt
@@ -35,7 +35,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
